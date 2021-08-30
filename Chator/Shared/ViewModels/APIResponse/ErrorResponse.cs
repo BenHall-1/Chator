4a9fd6de999927edb36c6626359b2a6946c02ed7
Ben Hall <ben@benh.codes>
2021-08-30 10:01:20 +0100

    Clear old project

--- Chator/Shared/ViewModels/APIResponse/ErrorResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Chator.Shared.ViewModels.APIResponse
-{
-    public class ErrorResponse
-    {
-        public int Status { get; set; }
-
-        public string Error { get; set; }
-    }
-}
